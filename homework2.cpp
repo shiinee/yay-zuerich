bc2c230f8f80dafa45f3b20a2e8b442374ec37f1
Skyler Leigh Amador <nevermaker@gmail.com>
2016-03-27 15:05:18 +0200

    Some flags and a mountain wow

--- homework2.cpp
+++ homework2.cpp
@@ -9,7 +9,7 @@ using namespace std;
 // Global Variable Section
 // List all of your global variables, stuctures and other definition here
 // Window display size
-GLsizei winWidth = 400, winHeight = 400;
+GLsizei winWidth = 800, winHeight = 600;
 
 // Initialize method
 void init(void)
@@ -25,19 +25,92 @@ void init(void)
 	glMatrixMode(GL_PROJECTION);
 	gluOrtho2D(-(GLdouble)winWidth / 2, (GLdouble)winWidth / 2, -(GLdouble)winHeight / 2, (GLdouble)winHeight / 2);
 }
-void otherCode()
-{
-	// Your other code here
+
+void drawSwissFlag(int from_x, int from_y, int size) {
+	// Red rectangle
+	// NOTE: The Swiss flag is square. So there's only one size measurement.
+	glColor3f(1.0, 0.0, 0.0);
+	glRecti(from_x, from_y, from_x + size, from_y + size);
+
+	// White plus (intersection of two rectangles)
+	int plus_weight = size/5;
+	int plus_left = from_x + plus_weight;
+	int plus_right = from_x + size - plus_weight;
+	int plus_top = from_y + size - plus_weight;
+	int plus_bottom = from_y + plus_weight;
+
+	glColor3f(1.0, 1.0, 1.0);
+	glRecti(plus_left, plus_bottom + plus_weight, plus_right, plus_top - plus_weight);
+	glRecti(plus_left + plus_weight, plus_bottom, plus_right - plus_weight, plus_top);
 }
+
+void drawZuerichFlag(int from_x, int from_y, int size) {
+	// White rectangle
+	// NOTE: The flag of Zuerich city is also square. Squares rock.
+	glColor3f(1.0, 1.0, 1.0);
+	glRecti(from_x, from_y, from_x + size, from_y + size);
+
+	// Blue triangle
+	glColor3f(0.0, 0.0, 1.0);
+	GLuint triangle;
+	glNewList(triangle, GL_COMPILE);
+	glBegin(GL_POLYGON);
+	glVertex2i(from_x, from_y);
+	glVertex2i(from_x + size, from_y);
+	glVertex2i(from_x, from_y + size);
+	glEnd();
+	glEndList();
+	glCallList(triangle);
+}
+
+void drawFlagpole(int from_x, int from_y, int width, int height) {
+	// Skinny black rectangle
+	glColor3f(0.0, 0.0, 0.0);
+	glRecti(from_x, from_y, from_x + width, from_y + height);
+}
+
+void drawMountain(int peak_x, int peak_y, int width, int height, float lightness) {
+	GLuint mountain, peak;
+
+	// Set shade of grey based on lightness
+	glColor3f(lightness, lightness, lightness);
+	
+	// Grey triangle
+	glNewList(mountain, GL_COMPILE);
+	glBegin(GL_POLYGON);
+	glVertex2i(peak_x - width/2, peak_y - height);
+	glVertex2i(peak_x, peak_y);
+	glVertex2i(peak_x + width/2, peak_y - height);
+	glEnd();
+	glEndList();
+	glCallList(mountain);
+
+	// Snowy peak (white triangle)
+	glColor3f(1.0, 1.0, 1.0);
+	glNewList(peak, GL_COMPILE);
+	glBegin(GL_POLYGON);
+	glVertex2i(peak_x - width/6, peak_y - height/3);
+	glVertex2i(peak_x, peak_y);
+	glVertex2i(peak_x + width/6, peak_y - height/3);
+	glEnd();
+	glEndList();
+	glCallList(peak);
+}
+
+
 // Generate the Graphics
 void displayFcn(void)
 {
 	// Clear display window.
 	glClear(GL_COLOR_BUFFER_BIT);
-	// Set  graphic objects color to Red or change for your choice
-	glColor3f(1.0, 0.0, 0.0);
-	// Your graphics code here  
-	
+
+	drawMountain(0, 200, 200, 200, 0.6);
+
+	drawZuerichFlag(-350, 50, 100);
+	drawFlagpole(-355, -200, 5, 350);
+
+	drawSwissFlag(250, 50, 100);
+	drawFlagpole(350, -200, 5, 350);
 
 	// Execute OpenGL functions
 	glFlush();
@@ -60,7 +133,7 @@ int main(int argc, char** argv)
 	// Set Window Size
 	glutInitWindowSize(winWidth, winHeight);
 	// Set Window Title
-	glutCreateWindow("CMSC405 Template");
+	glutCreateWindow("Skyler Amador - Homework 2");
 	// Initialize
 	init();
 	// Display function call
